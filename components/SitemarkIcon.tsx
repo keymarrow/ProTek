a9d54402b326fedebf439d11edab0efe73da6b77
keymarrow <pkimaro15@gmail.com>
2025-08-28 20:56:43 +0300

    back home option

--- components/SitemarkIcon.tsx
+++ components/SitemarkIcon.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 
 export default function SitemarkIcon() {
   return (
-    <img
-      src="/ProTek.svg"
-      alt="ProTek Logo"
-      style={{ height: 30, width: 'auto' }}
-    />
+    <a href="/" aria-label="Go to homepage" style={{ display: 'inline-flex', alignItems: 'center' }}>
+      <img src="/ProTek.svg" alt="ProTek Logo" style={{ height: 30, width: 'auto' }} />
+    </a>
   );
 }
