4ada596eaf97a69c858ef8a3f7d4f2c40b0b63bd
keymarrow <pkimaro15@gmail.com>
2025-08-28 20:36:33 +0300

    final git changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,16 +4,15 @@ import * as React from 'react';
 export default function AdminRedirect() {
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
-      window.location.replace('/admin/index.html');
+      window.location.replace('/cms/index.html');
     }
   }, []);
   return (
     <div style={{ padding: 24 }}>
       <p>Loading CMS…</p>
       <p>
-        If it does not load, open <a href="/admin/index.html">/admin/index.html</a>.
+        If it does not load, open <a href="/cms/index.html">/cms/index.html</a>.
       </p>
     </div>
   );
 }
-
